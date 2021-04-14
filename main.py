0a0af62e4b099a26d9be74e73600706521622e0a
Dirac1562 <71075475+Dirac1562@users.noreply.github.com>
2021-04-14 21:18:08 +0200

    [UPDATE] uadmin functions to work properly

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ while True:
     main_menu()
     choice = input("What number do you choose?").lower()
     if choice == "1" or choice == "one":
-        sign_up()
+        user = sign_up()
     elif choice == "2" or choice == "two":
-        login()
+        user = login()
         option = input("What number do you choose?").lower()
         if option == "1" or choice == "one":
             Quiz()
@@ -37,13 +37,13 @@ while True:
             print("Your score is: ")
         elif option == "4" or choice == "four":
             dict1 = {}
-            Administrator.add_question(dict1)
+            user.add_question(dict1)
         elif option == "5" or choice == "five":
             dict2 = {}
-            Administrator.add_new_administrator(dict2)
+            user.add_new_administrator(dict2)
         elif option == "6" or choice == "six":
             dict3 = {}
-            Administrator.remove_user(dict3)
+            user.remove_user(dict3)
         elif option == "7" or choice == "seven":
             get_the_user()
         elif option == "8" or choice == "eight" or choice == "3" or choice == "three":
